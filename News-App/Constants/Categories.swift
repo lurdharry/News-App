fddb703582d51469402b44102d2036c3b62c03d3
lurdharry <azeezhammed57@gmail.com>
2023-01-18 07:14:16 -0500

    feat: add category list to table view

---
+++ News-App/Constants/Categories.swift
@@ -0,0 +1,18 @@
+//
+//  Categories.swift
+//  News-App
+//
+//  Created by Hammed on 18/01/2023.
+//
+
+import Foundation
+
+
+let Categories  = [
+    Category(title: "Top Headline", url: "nnnnn"),
+    Category(title: "Telsa", url: "nnnnn"),
+    Category(title: "Apple", url: "nnnnn"),
+    Category(title: "Tech Crunch", url: "nnnnn"),
+    Category(title: "Wall Street Journal", url: "nnnnn"),
+]
+
---
+++ News-App/Model/CategoryList.swift
@@ -0,0 +1,13 @@
+//
+//  CategoryList.swift
+//  News-App
+//
+//  Created by Hammed on 18/01/2023.
+//
+
+import Foundation
+
+struct Category {
+    let title: String
+    let url: String
+}
