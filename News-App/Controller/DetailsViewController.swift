1155fa15ffbf012f09af66afbba98f97ba8fa09c
lurdharry <azeezhammed57@gmail.com>
2023-02-06 14:57:38 -0500

    chore: format date

--- News-App/Controller/DetailsViewController.swift
+++ News-App/Controller/DetailsViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 import Alamofire
 import AlamofireImage
 
+
 class DetailsViewController: UIViewController {
     
     @IBOutlet weak var topImage: UIImageView!
     @IBOutlet weak var newsTitle: UILabel!
+    @IBOutlet weak var newsContent: UILabel!
+    @IBOutlet weak var dateText: UILabel!
+    @IBOutlet weak var author: UILabel!
     
+    let dateFormatterGet = DateFormatter()
+    let dateFormatter = DateFormatter()
+
     var details : News? {
         didSet{
             let downloadURL = URL(string:(details?.urlToImage)!)!
@@ -27,6 +34,16 @@ class DetailsViewController: UIViewController {
         super.viewDidLoad()
         title = details?.source.name
         newsTitle.text = details?.title
+        newsContent.text = details?.description
+ 
+        dateFormatterGet.dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'"
+
+        if let date  = dateFormatterGet.date(from: (details?.publishedAt)!), let postAuthor = details?.author {
+            author.text = "By: \(postAuthor)"
+            dateFormatter.dateFormat = "EEEE, MMM d, yyyy"
+            dateText.text = dateFormatter.string(from: date)
+        }
+
     }
 
 }
